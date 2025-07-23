de96148201846d9061c9c1bf7b835dcc4495aa9b
anna-idell <woo19014@byui.edu>
2025-07-22 23:41:00 -0600

    Upload semester project

--- week_11/Data Search.R
+++ week_11/Data Search.R
@@ -1,5 +1,30 @@
 
 ## Fertility Rate - Our World in Data
 
-fertility_rate <- read.csv(https://ourworldindata.org/grapher/children-born-per-woman.csv?v=1&csvType=full&useColumnShortNames=true)
+library(tidyverse)
+library(haven)
 
+# Is there a difference between the two sexes in views about children?
+
+
+GSS <- read_dta("C:/Users/annab/Downloads/GSS_stata/GSS_stata/gss7224_r1.dta")
+
+# age, sex, childs, chldidel, kidnofre, happy, hapmar, 
+
+GSS2 <- GSS %>% 
+  select(c(age, sex, childs, chldidel, agekdbrn, kidnofre, happy, 
+           hapmar, adoptkid, adoption)) %>% 
+  filter(!is.na(sex))
+  
+ideal <- GSS2 %>% 
+  select(c(age, sex, chldidel)) %>% 
+  filter(!is.na(chldidel))
+
+freedom <- GSS2 %>% 
+  select(c(age, sex, kidnofre)) %>% 
+  filter(!is.na(kidnofre))
+
+happiness <- GSS2 %>% 
+  select(c(age, sex, childs, happy)) %>% 
+  filter(!is.na(childs)) %>% 
+  filter(!is.na(happy))
--- week_13/README.md
+++ week_13/README.md
@@ -1,1 +1,5 @@
 # Week 13 Notes
+
+Interactive Map Leaflet
+https://posit.byui.edu/content/5d9a1c63-0337-4966-9679-413f823c004e
+
